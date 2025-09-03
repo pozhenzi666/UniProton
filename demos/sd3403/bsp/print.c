c483e051bd7ad893d9fe098a89fe9646a3b8877f
Wanming Hu <huwanming@huawei.com>
2025-09-03 11:26:06 +0800

    uniproton: support sd3403
    Support uniproton to run on sd3403/hieulerpi with oEE mica.
    
    Signed-off-by: Wanming Hu <huwanming@huawei.com>

---
+++ demos/sd3403/bsp/print.c
@@ -0,0 +1,64 @@
+#include <stdarg.h>
+#include "prt_typedef.h"
+#include "cpu_config.h"
+#include "securec.h"
+#include "pl011.h"
+
+#define WRITE_UINT32(value, addr) {          \
+    OS_EMBED_ASM("DSB SY");                  \
+    *(volatile U32 *)(addr) = (U32)(value);  \
+}
+
+#define GET_UINT32(addr) ({                  \
+    U32 r = *(volatile U32 *)(addr);         \
+    OS_EMBED_ASM("DSB SY"); r;               \
+})
+
+typedef U32 (*PrintFunc)(const char *format, va_list vaList);
+#define OS_MAX_SHOW_LEN 0x200
+
+void uart_poll_send(unsigned char ch)
+{
+    while (UART_REG(UART_FR) & UART_TXFF) {
+        asm volatile("yield" ::: "memory");
+    }
+    UART_REG(UART_DR) = ch;
+}
+
+void TestPutc(unsigned char ch)
+{
+    uart_poll_send(ch);
+    if(ch == '\n') {
+        uart_poll_send('\r');
+    }
+}
+int TestPrintf(const char *format, va_list vaList)
+{
+    int len;
+    char buff[OS_MAX_SHOW_LEN] = {0};
+    char *str = buff;
+
+    len = vsnprintf_s(buff, OS_MAX_SHOW_LEN, OS_MAX_SHOW_LEN, format, vaList);
+    if (len == -1) {
+        return len;
+    }
+
+    while (*str != '\0') {
+        TestPutc(*str);
+        str++;
+    }
+
+    return OS_OK;
+}
+
+U32 PRT_Printf(const char *format, ...)
+{
+    va_list vaList;
+    S32 count;
+
+    va_start(vaList, format);
+    count = TestPrintf(format, vaList);
+    va_end(vaList);
+
+    return count;
+}
---
+++ demos/sd3403/build/build_static.sh
@@ -0,0 +1,23 @@
+if [ ! -d "../include" ]; then
+    git clone https://gitee.com/openeuler/libboundscheck.git
+    if [ $? != 0 ]; then
+        tar -xvf libboundscheck.tar
+    fi
+    cp libboundscheck/include/* ../../../platform/libboundscheck/include
+    cp libboundscheck/src/* ../../../platform/libboundscheck/src
+
+    mkdir ../include
+    cp -r ../../../src/include/uapi/* ../include
+    cp libboundscheck/include/* ../include
+    rm -rf libboundscheck
+fi
+
+pushd ./../../../
+
+python3 build.py $1
+cp output/UniProton/lib/$1/* demos/sd3403/libs
+cp output/libboundscheck/lib/$1/* demos/sd3403/libs
+cp -r output/libc demos/sd3403/include
+cp -r src/include/uapi/* demos/sd3403/include
+cp build/uniproton_config/config_armv8_sd3403/prt_buildef.h demos/$1/include/
+popd
